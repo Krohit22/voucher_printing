44bb993b2db72956b14be37cce06e646cccc43e9
Krohit22 <rajpurohitkrish45666@gmail.com>
2024-08-30 19:47:34 +0530

    C6

--- app/AddItem.tsx
+++ app/AddItem.tsx
@@ -1,10 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { View,StyleSheet,Text,TextInput, TouchableOpacity, Modal} from "react-native";
+import { View,StyleSheet,Text,TextInput, TouchableOpacity, Modal, FlatList} from "react-native";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import * as SplashScreen from "expo-splash-screen";
+import Feather from '@expo/vector-icons/Feather';
 import { useFonts } from "expo-font";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import SaveButton from "../components/saveButtton";
+const data =[
+    {Unit:"Kilogram",code:'23'},
+    {Unit:"Gram",code:'344'},
+    {Unit:"Metre",code:'666'},
+    {Unit:"Centimetre",code:'767'},
+    {Unit:"Litre",code:'343'},
+    {Unit:"Mililitre",code:'221'},
+    {Unit:"Millimetre",code:'765'},
+    {Unit:"Milligram",code:'386'},
+    {Unit:"Foot",code:'385'},
+    {Unit:"Inch",code:'384'},
+    {Unit:"Quart",code:'383'},
+]
 export default function AddItem(){
+    
     const [IsUnitButton,SetUnitButton] = useState(false)
+    const [selectedUnit,SetSeletedUnit] = useState('');
+    const[IsAddNewItemBtn,SetAddNewItemBtn]= useState(false)
+    const [isdata,setdata] = useState(data);
+    const onsearch = (txt: any) => {
+      let tempData = data.filter((item) => {
+        return item.Unit.toLowerCase().indexOf(txt.toLowerCase()) > -1;
+      });
+      setdata(tempData);
+    };
     const [fontloaded] = useFonts({
         Poppins:require('../assets/fonts/Poppins-Medium.ttf'),
         love: require('../assets/fonts/PlaywriteSK-ExtraLight.ttf')
@@ -26,27 +52,58 @@ export default function AddItem(){
         <View style={styles.AddItemsContainer}>
 
             {/* add item name */}
-            <View style={styles.itemsnameConatainer}><TextInput style={styles.itemsname}></TextInput></View>
+            <View style={styles.itemsnameConatainer}><TextInput style={styles.itemsname} placeholder="Item Name*"></TextInput><TouchableOpacity style={styles.addNewItem} onPress={()=>SetAddNewItemBtn(true)}><AntDesign name="plus" size={24} color="black" /></TouchableOpacity></View>
 
             {/* add item qty and unit */}
-            <View style={styles.itemsQtyAndUnitContainer}><TextInput style={styles.QuentyInput}></TextInput><TextInput style={styles.UnitInput}></TextInput></View>   
-            <TouchableOpacity style={styles.AddUnitButton} onPress={()=>SetUnitButton(true)}><MaterialIcons name="keyboard-arrow-up" size={35} color="black" /></TouchableOpacity>
+            <View style={styles.itemsQtyAndUnitContainer}><TextInput style={styles.QuentyInput} placeholder="Quantity*"></TextInput>
+                <TouchableOpacity style={styles.AddUnitButton} onPress={()=>SetUnitButton(true)}><Text style={styles.AddUnitButtonText}>Unit*</Text><View style={styles.AddUnitButtonIcon}><MaterialIcons name="keyboard-arrow-up" size={35} color="black" /></View></TouchableOpacity>
+            </View>   
+            
 
             {/* add item Rate */}
-            <View style={styles.RateInputConatainer}><TextInput style={styles.RateInput}></TextInput></View>
+            <View style={styles.RateInputConatainer}><TextInput style={styles.RateInput} placeholder="Rate*"></TextInput></View>
 
-            {/* add item HSN Code*/}
-            <View style={styles.HSNCodeInputConatainer}><TextInput style={styles.HSNCodeInput}></TextInput></View>
+            
 
             {/* Add unit modal */}
-            <Modal visible={IsUnitButton} transparent={true}>
+            <Modal visible={IsUnitButton} transparent={true} >
                 <View style={styles.UnitModalConatainer}>
                     <View style={styles.UnitModalBackground}>
-                        <View><Text style={styles.AddUnitModalTitle}>Change Item Unit</Text></View>
+                        <View style={styles.UnitModalConatainerHeader}><Text style={styles.AddUnitModalTitle}>Change Item Unit</Text><TouchableOpacity style={styles.UnitModalConatainerCloseButton} onPress={()=>SetUnitButton(false)}><Feather name="x" size={24} color="black" /></TouchableOpacity></View>
+                        <View>
+                            <TextInput placeholder="Search" style={styles.UniteSearch} onChangeText={(txt)=>onsearch(txt)}></TextInput>
+                            
+                                <FlatList data={isdata} renderItem={({item,index})=>{
+                                    return(
+                                       
+                                            <TouchableOpacity style={styles.UnitList} onPress={()=>{
+                                                SetSeletedUnit(item.Unit)
+                                                SetUnitButton(false)
+                                            }}>
+                                                <Text>{item.Unit}</Text>
+                                            </TouchableOpacity>
+                                       
+                                    );
+                                }} style={styles.UnitListContainer}/>
+                            
 
+                            </View>
                     </View>
                 </View>
             </Modal>
+
+            {/* add new item model */}
+            <Modal visible={IsAddNewItemBtn} transparent={true}>
+                <View style={styles.AddNewItemConatainer}>
+                    <View style={styles.AddItemsContainerBackground}>
+                                <View style={styles.AddNewItemheader}><Text style={styles.AddNewItemheaderText}>Craete a Items</Text><TouchableOpacity style={styles.closebtnAddItems} onPress={()=>SetAddNewItemBtn(false)}><Feather name="x" size={28} color="#800020" /></TouchableOpacity></View>
+                                <View><TextInput style={styles.addNewItemInputs} placeholder="Item Name*"></TextInput></View>
+                                <View><TextInput style={styles.addNewItemInputs} placeholder="Item Category*"></TextInput></View>
+                                <View><TextInput style={styles.addNewItemInputs} placeholder="HSN Code*"></TextInput></View>
+                                <View style={styles.saveButton}><SaveButton/></View>
+                    </View>
+                </View>
+            </Modal>  
         </View>
     );
 }
@@ -61,7 +118,8 @@ const styles = StyleSheet.create({
         width:'96%',
         borderRadius:8,
         marginLeft:8,
-        marginTop:20
+        marginTop:20,
+        paddingLeft:10
     },
     itemsnameConatainer:{
         width:'100%',
@@ -77,17 +135,8 @@ const styles = StyleSheet.create({
         borderWidth:1,
         borderRadius:8,
         marginLeft:8,
-        marginTop:10
-    },
-    UnitInput:{
-        width:192,
-        height:45,
-        borderColor:'#938F8F',
-        borderWidth:1,
-        borderRadius:8,
-        marginLeft:10,
         marginTop:10,
-        
+        paddingLeft:10
     },
     RateInputConatainer:{
         width:'100%',
@@ -99,7 +148,8 @@ const styles = StyleSheet.create({
         width:'96%',
         borderRadius:8,
         marginLeft:8,
-        marginTop:10
+        marginTop:10,
+        paddingLeft:10
     },
     HSNCodeInputConatainer:{
         width:'100%',
@@ -111,12 +161,21 @@ const styles = StyleSheet.create({
         width:'96%',
         borderRadius:8,
         marginLeft:8,
-        marginTop:10
+        marginTop:10,
+        paddingLeft:10
     },
     AddUnitButton:{
-        position:'absolute',
-        right:20,
-        top:80
+        width:192,
+        height:45,
+        borderColor:'#938F8F',
+        borderWidth:1,
+        borderRadius:8,
+        marginLeft:10,
+        marginTop:10,
+        alignItems:'center',
+        flexDirection:'row',
+        paddingLeft:10,
+        
     },
     UnitModalConatainer:{
         backgroundColor: 'rgba(0, 0, 0, 0.08)',
@@ -127,16 +186,101 @@ const styles = StyleSheet.create({
         margin: 2,
         padding: 20,
         borderRadius: 10,
-        height: 500,
-        marginTop:310
+        height: 550,
+        marginTop:310,
+        overflow:'hidden'
     },
     AddUnitModalTitle:{
         fontSize:20,
+        fontFamily:'Poppins',  
+    },
+    AddUnitButtonIcon:{
+        marginLeft:100
+    },
+    AddUnitButtonText:{
+        fontSize:14,
+        fontFamily:'Poppins'
+    },
+    UnitModalConatainerCloseButton:{
+        marginLeft:170
+    },
+    UnitModalConatainerHeader:{
+        flexDirection:'row'
+    },
+    UniteSearch:{
+        padding:10,
+        paddingLeft:12,
+        borderWidth:0.5,
+        borderColor:'black',
+        width:300,
+        marginLeft:35,
+        marginTop:20,
+        borderRadius:8
+
+    },
+    UnitList:{
+        paddingBottom:15,
+        paddingTop:10,
+        borderBottomColor:'black',
+        borderBottomWidth:1,
+        width:300,
+        height:45,
+
+        marginLeft:40
+    },
+    UnitListContainer:{
+        marginTop:20,
+    
+    },
+    addNewItem:{
+        position:'absolute',
+        right:15,
+        top:30
+    },
+    AddNewItemConatainer:{
+        backgroundColor: 'rgba(0, 0, 0, 0.08)',
+        flex:1
+    },
+    AddItemsContainerBackground:{
+        backgroundColor:'white',
+        margin:5,
+        height:400,
+        marginTop:455,
+        borderRadius:20,
+        padding:20
+    },
+    closebtnAddItems:{
+        position:'absolute',
+        right:5,
+        top:6
+    },
+    AddNewItemheader:{
+        flexDirection:'row',
+        justifyContent:'center',
+        
+    },
+    AddNewItemheaderText:{
+        color:'#800020',
         fontFamily:'Poppins',
+        fontSize:24,
+        marginBottom:10,
+
         
-        
-        
+    },
+    addNewItemInputs:{
+        width:'95%',
+        borderColor:'black',
+        borderWidth:1,
+        padding:10,
+        marginLeft:10,
+        marginTop:15,
+        borderRadius:8
+    },
+    saveButton:{
+        marginTop:50
     }
+
+
 
 
 })
